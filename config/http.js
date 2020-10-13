d11a434f02325a93a812d3488d7a77c8a69c377e
imaluris <andrea.castellaniperelli@gmail.com>
2020-10-13 14:26:28 +0200

    error

--- config/http.js
+++ config/http.js
@@ -23,16 +23,11 @@ module.exports.http = {
 
   middleware: {
 
-    passportInit    : require('passport').initialize(),
-    passportSession : require('passport').session(),
 
      order: [
             'startRequestTimer',
             'cookieParser',
             'session',
-            'passportInit',
-            'passportSession',
-            'myRequestLogger',
             'bodyParser',
             'handleBodyParserError',
             'compress',
